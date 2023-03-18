980947e2854c2923937de89da035b49ebb8066f1
iSE <77599027+ttrangnguyen@users.noreply.github.com>
2023-03-18 19:16:30 +0900

    remove redundant code

--- jitvul_model/FastRGCN.py
+++ jitvul_model/FastRGCN.py
@@ -40,44 +40,7 @@ class FastRGCN(torch.nn.Module):
             x = global_max_pool(x,  torch.zeros(x.shape[0], dtype=int, device=x.device))
         else:
             x = global_add_pool(x,  torch.zeros(x.shape[0], dtype=int, device=x.device))
-            # 3. Apply a final classifier
-        x = F.dropout(x, p=self.dropout, training=self.training)
-        x = self.lin(x)
-        return x
-
-class CuGraphRGCN(torch.nn.Module):
-    def __init__(self, in_channels, hidden_channels,edge_dim, num_relations = 4, dropout = 0.1, num_of_layers = 2, graph_readout_func = "add"):
-        super(CuGraphRGCN, self).__init__()
-        torch.manual_seed(12345)
-        self.num_layers = num_of_layers
-        self.num_of_relations = num_relations
-        self.dropout = dropout
-        self.graph_readout_func = graph_readout_func
-        for i in range(self.num_layers):
-            if i == 0:
-                exec('self.conv_{} = CuGraphRGCNConv(in_channels, hidden_channels,num_relations=self.num_of_relations)'.format(i))
-            else:
-                exec('self.conv_{} = CuGraphRGCNConv(hidden_channels, hidden_channels,num_relations=self.num_of_relations)'.format(i))
-        self.relu = ReLU(inplace=True)
-        self.lin = Linear(hidden_channels, 2)
-
-    def forward(self, x, edge_index, edge_type, edge_attr):
-        # 1. Obtain node embeddings
-        for i in range(self.num_layers):
-            if i < self.num_layers - 1:
-                x = eval('self.conv_{}(x, torch.to_csc(edge_index), edge_type)'.format(i))
-                x = self.relu(x)
-            else:
-                x = eval('self.conv_{}(x, torch.to_csc(edge_index), edge_type)'.format(i))
-
-        # 2. Readout layer
-        if self.graph_readout_func == "average":
-            x = global_mean_pool(x,  torch.zeros(x.shape[0], dtype=int, device=x.device))
-        if self.graph_readout_func == "max":
-            x = global_max_pool(x,  torch.zeros(x.shape[0], dtype=int, device=x.device))
-        else:
-            x = global_add_pool(x,  torch.zeros(x.shape[0], dtype=int, device=x.device))
-            # 3. Apply a final classifier
+        # 3. Apply a final classifier
         x = F.dropout(x, p=self.dropout, training=self.training)
         x = self.lin(x)
         return x
